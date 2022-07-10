d1bb4b7d4b3ad75ef4213cd643e09e94ebcc3b46
Samuel <42385434+samuelV102@users.noreply.github.com>
2022-07-10 16:30:24 -0300

    -> Project identification in the package and composer files. -> Install and config front-end packages. -> Development settings. -> Remove stimulus framework. -> General adjustments.

---
+++ app/docs/index.js
@@ -0,0 +1,5 @@
+import './styles/index.scss'
+
+$(() => {
+    console.log('Hello Docs')
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
-const Encore = require('@symfony/webpack-encore');
+/* eslint-disable spaced-comment */
+const Encore = require('@symfony/webpack-encore')
+
+//////////////// ADMIN CONFIG //////////////////
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
 }
 
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath('public/build/admin/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath('/build/admin/')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
@@ -20,10 +23,31 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('app', './app/admin/index.js')
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    .copyFiles({
+        from: './app/admin/images',
+
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]'
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
+
+    .configureImageRule({
+        // tell Webpack it should consider inlining
+        type: 'asset'
+        //maxSize: 4 * 1024, // 4 kb - the default is 8kb
+    })
+
+    .configureFontRule({
+        type: 'asset'
+        //maxSize: 4 * 1024
+    })
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -46,17 +70,19 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     .configureBabel((config) => {
-        config.plugins.push('@babel/plugin-proposal-class-properties');
+        config.plugins.push('@babel/plugin-proposal-class-properties')
     })
 
     // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = 3;
+        config.useBuiltIns = 'usage'
+        config.corejs = 3
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
+
+    .enablePostCssLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -69,7 +95,124 @@ Encore
     //.enableIntegrityHashes(Encore.isProduction())
 
     // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
-;
+    .autoProvidejQuery()
 
-module.exports = Encore.getWebpackConfig();
+// build the first configuration
+const adminConfig = Encore.getWebpackConfig()
+
+// Set a unique name for the config (needed later!)
+adminConfig.name = 'admin-config'
+
+// reset Encore to build the second config
+Encore.reset()
+
+///////////////////////////////////////////////////////
+
+//////////////// DOCS CONFIG //////////////////
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
+}
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/docs/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build/docs/')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('app', './app/docs/index.js')
+
+    .copyFiles({
+        from: './app/docs/images',
+
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]'
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
+
+    .configureImageRule({
+        // tell Webpack it should consider inlining
+        type: 'asset'
+        //maxSize: 4 * 1024, // 4 kb - the default is 8kb
+    })
+
+    .configureFontRule({
+        type: 'asset'
+        //maxSize: 4 * 1024
+    })
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties')
+    })
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage'
+        config.corejs = 3
+    })
+
+    // enables Sass/SCSS support
+    .enableSassLoader()
+
+    .enablePostCssLoader()
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // uncomment if you use React
+    //.enableReactPreset()
+
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
+
+    // uncomment if you're having problems with a jQuery plugin
+    .autoProvidejQuery()
+
+// build the second configuration
+const docsConfig = Encore.getWebpackConfig()
+
+// Set a unique name for the config (needed later!)
+docsConfig.name = 'docs-config'
+
+// reset Encore to build the second config
+Encore.reset()
+
+///////////////////////////////////////////////////////
+
+module.exports = [adminConfig, docsConfig]
